38de86bdc03877de7e58318b26c6ae9b4a9586b1
Ahmed Ghozy <143905628+ahmedmohamed-mu@users.noreply.github.com>
2026-08-28 23:36:30 +0300

    Add manifest file for MU Construction Project module

---
+++ mu_construction_project_bootstrap/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "MU Construction Contract and Project Bootstrap",
+    "summary": "Controlled handover from awarded tender to standard project, contract, and BOQs",
+    "version": "19.0.1.0.0",
+    "category": "Services/Project",
+    "author": "MU Constructions General",
+    "license": "LGPL-3",
+    "depends": ["mu_construction_tender_estimation", "project_enterprise", "mail"],
+    "data": [
+        "security/bootstrap_security.xml",
+        "security/ir.model.access.csv",
+        "data/bootstrap_sequence.xml",
+        "views/bootstrap_views.xml",
+        "views/tender_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
