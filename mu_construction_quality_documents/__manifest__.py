f83d9677927d2401932c244c7dbd97d77c48f5f9
Ahmed Ghozy <143905628+ahmedmohamed-mu@users.noreply.github.com>
2026-08-30 20:38:04 +0300

    Remove manifest details from __manifest__.py

--- mu_construction_quality_documents/__manifest__.py
+++ mu_construction_quality_documents/__manifest__.py
@@ -16,22 +16,3 @@
     "installable": True,
     "application": False,
 }
-{
-    "name": "MU Construction QA/QC and Document Control",
-    "summary": "Drawing, RFI, submittal, transmittal, ITP, MIR, WIR, NCR, and snag controls",
-    "version": "19.0.1.0.0",
-    "category": "Services/Project",
-    "author": "MU Constructions General",
-    "license": "LGPL-3",
-    "depends": ["mu_construction_site_execution", "documents", "quality_control", "stock", "mail"],
-    "data": [
-        "security/quality_document_security.xml",
-        "security/ir.model.access.csv",
-        "data/quality_document_sequences.xml",
-        "views/document_control_views.xml",
-        "views/quality_control_views.xml",
-    ],
-    "installable": True,
-    "application": False,
-}
-
